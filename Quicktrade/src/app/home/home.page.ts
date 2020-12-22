7513256a2c3a880eb9fbcdf4efdc63edc1652b99
jesagi <jesagi@floridauniversitaria.es>
2020-12-22 23:05:06 +0100

    Mejora pantalla

--- Quicktrade/src/app/home/home.page.ts
+++ Quicktrade/src/app/home/home.page.ts
@@ -7,7 +7,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  oculto : boolean = false;
+  tecOculto : boolean = false;
+  hogOculto : boolean = false;
+  InmOculto : boolean = false;
+  motOculto : boolean = false;
+
+  TecnoOculto() : void {
+    this.tecOculto = true;
+    this.hogOculto = false; 
+    this.InmOculto = false;
+    this.motOculto = false;
+  }
+  HogarOculto() : void {
+    this.tecOculto = false;
+    this.hogOculto = true; 
+    this.InmOculto = false;
+    this.motOculto = false;
+  }
+  InmoOculto() : void {
+    this.tecOculto = false;
+    this.hogOculto = false; 
+    this.InmOculto = true;
+    this.motOculto = false; 
+  }
+  MotorOculto() : void {
+    this.tecOculto = false;
+    this.hogOculto = false; 
+    this.InmOculto = false;
+    this.motOculto = true; 
+  }
+
   constructor() {}
 
 }
