e42f7902c0e263e9969eecdba9eb4d699367b87e
jesagi <jesagi@floridauniversitaria.es>
2021-01-21 21:09:51 +0100

    Probar crear objeto

--- Quicktrade/src/app/listaarticulos/listaarticulos.page.ts
+++ Quicktrade/src/app/listaarticulos/listaarticulos.page.ts
@@ -18,26 +18,21 @@ export class ListaarticulosPage implements OnInit {
   }
 
   ngOnInit() {
-    let objeto = {
-      id: 4,
-      nombre: "string",
-      descripcion: "string",
-      precio: 3,
-      tipo: "string",
-    }
-    
-    let ref = this._articuloService.getArticulos();
-    console.log(objeto);
-    this.articulos.push(objeto);
-
-/*
-    ref.on("value", snapshot =>{
+    let ref= this._articuloService.getArticulos();
+    ref.once("value", snapshot => {
       snapshot.forEach(child => {
-        let value = child.val();
-        console.log(child.val());
+        let value = {
+          "id" : child.val().id,
+          "nombre" : child.val().nombre,
+          "descripcion" : child.val().descripcion,
+          "precio" : child.val().precio,
+          "tipo" : child.val().tipo,
+          "estado" : child.val().estado
+        };
+        console.log(value);
         this.articulos.push(value);
       })
-    })*/
+    })
   }
   
 
