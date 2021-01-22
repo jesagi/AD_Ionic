446a581fe444621169b04e475c17946700202aea
jesagi <jesagi@floridauniversitaria.es>
2021-01-22 12:11:07 +0100

    Solucionado

--- Quicktrade/src/app/listaarticulos/listaarticulos.page.ts
+++ Quicktrade/src/app/listaarticulos/listaarticulos.page.ts
@@ -9,7 +9,7 @@ import { ArticuloService } from '../services/articulo.service';
 })
 export class ListaarticulosPage implements OnInit {
 
-  articulos : (IArticulo | ITecnologia | IInmobiliaria | IMotor)[];
+  articulos : (IArticulo | ITecnologia | IInmobiliaria | IMotor) = [];
 
   
 
@@ -21,14 +21,7 @@ export class ListaarticulosPage implements OnInit {
     let ref= this._articuloService.getArticulos();
     ref.once("value", snapshot => {
       snapshot.forEach(child => {
-        let value = {
-          "id" : child.val().id,
-          "nombre" : child.val().nombre,
-          "descripcion" : child.val().descripcion,
-          "precio" : child.val().precio,
-          "tipo" : child.val().tipo,
-          "estado" : child.val().estado
-        };
+        let value = child.val()
         console.log(value);
         this.articulos.push(value);
       })
